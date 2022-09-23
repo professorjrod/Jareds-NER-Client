cdde2b6b967a9b32c31e601011e249c26a5eb70c
Jared <mumawjared@gmail.com>
2022-09-23 01:12:50 -0700

    text annotate dropdown

--- annotate/client/src/Annotate.js
+++ annotate/client/src/Annotate.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { TextAnnotator } from "react-text-annotate";
 const Annotate = () => {
   let { id } = useParams();
 
   let [text, setText] = useState("");
+
+  let [annotations, setAnnotations] = useState({
+    value: [{ start: 0, end: 0, tag: "" }],
+    tag: "",
+  });
 
   useEffect(() => {
     fetch(`/texts/${id}`)
@@ -12,7 +18,7 @@ const Annotate = () => {
       .then((data) => setText(data))
       .catch((err) => console.log(err));
   }, []);
-
+  console.log(annotations);
   return (
     <div className="flex">
       <Link className="breadcrumb" to={`/datasets/${text.dataset_id}`}>
@@ -20,6 +26,15 @@ const Annotate = () => {
       </Link>
       <div className="text-card">
         <div className="preview">
+          <select
+            onChange={(e) =>
+              setAnnotations({ ...annotations, tag: e.target.value })
+            }
+            value={annotations.tag}
+          >
+            <option value="Make">Make</option>
+            <option value="Model">Model</option>
+          </select>
           <h1>
             <strong>Preview</strong>
           </h1>
